752660341d252b3c8151229bdc8789f7df0e4a27
TwinkletoesZen <contact@twinkletoeszen.com>
2021-12-22 19:02:26 -0800

    Sucessful Download with Selenium is good and it does not restrict

--- downloader.py
+++ downloader.py
@@ -7,18 +7,18 @@ from time import sleep
 
 browser = webdriver.Chrome()
 
-browser.get("https://ca.finance.yahoo.com/quote/FM.TO/history?p=FM.TO")
+browser.get("https://query1.finance.yahoo.com/v7/finance/download/FM.TO?period1=1608692260&period2=1640228260&interval=1d&events=history&includeAdjustedClose=true")
 
-def Download():
-  try:
-    File = WebDriverWait(browser, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a/span'))
-    )
-    sleep(1)
-    try:
-      File.click()
-    except:
-      print("Download Failed")
+# def Download():
+#   try:
+#     File = WebDriverWait(browser, 10).until(
+#         EC.presence_of_element_located((By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a/span'))
+#     )
+#     sleep(1)
+#     try:
+#       File.click()
+#     except:
+#       print("Download Failed")
           
-  except:
-    print("It didn't work")
+#   except:
+#     print("It didn't work")
