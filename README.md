218c75b73063df47042b7a31c03f64223abb6136
TwinkletoesZen <contact@twinkletoeszen.com>
2021-12-22 19:09:32 -0800

    Added ReadMe; Cleaned Up Code

---
+++ README.md
@@ -0,0 +1,10 @@
+# File Downloader
+
+# Requirments
+1. Selenium
+`pip3 install selenium`
+
+# Library Used
+1. Selenium
+
+
--- downloader.py
+++ downloader.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait, Select
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait, Select
+# from selenium.webdriver.support import expected_conditions as EC
+# from time import sleep
 
 browser = webdriver.Chrome()
 
