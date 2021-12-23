46298ad996f77f96e27c0142f1a9176d7be25968
TwinkletoesZen <contact@twinkletoeszen.com>
2021-12-22 18:57:06 -0800

    Selenium Base Code

--- downloader.py
+++ downloader.py
@@ -1,6 +1,24 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
-url = "https://query1.finance.yahoo.com/v7/finance/download/FM.TO?period1=1607647159&period2=1639183159&interval=1d&events=history&includeAdjustedClose=true"
+browser = webdriver.Chrome()
 
-r = requests.get(url, allow_redirects=True)
-open('Data_2.csv', 'wb').write(r.content)
+browser.get("https://ca.finance.yahoo.com/quote/FM.TO/history?p=FM.TO")
+
+def Download():
+  try:
+    File = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[1]/div[2]/span[2]/a/span'))
+    )
+    sleep(1)
+    try:
+      File.click()
+    except:
+      print("Download Failed")
+          
+  except:
+    print("It didn't work")
